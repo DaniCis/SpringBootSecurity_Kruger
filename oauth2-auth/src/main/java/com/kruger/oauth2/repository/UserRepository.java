887bb192c7fa13a22ef34fcc59b6b7f00abd59a1
DaniCis <danicisneros2@hotmail.com>
2023-01-05 15:59:14 -0500

    videos finished

---
+++ oauth2-auth/src/main/java/com/kruger/oauth2/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kruger.oauth2.repository;
+
+import com.kruger.oauth2.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
